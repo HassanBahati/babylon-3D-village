b5d45f89ae21691a9c57e4be2a767e0ab0c9a1ab
HassanBahati <mukisabahati@gmail.com>
2021-07-08 13:37:26 +0300

    editted project structure

---
+++ index.js
@@ -0,0 +1,42 @@
+
+const canvas = document.getElementById("renderCanvas"); // Get the canvas element
+const engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
+
+// Add your code here matching the playground format
+const createScene = function () {
+
+    const scene = new BABYLON.Scene(engine);  
+
+    // create box 
+    const box = BABYLON.MeshBuilder.CreateBox("box", {})
+
+    // create  a camera 
+    const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 15, new BABYLON.Vector3(0, 0, 0));
+    camera.attachControl(canvas, true);
+
+    // add light 
+    const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(1, 1, 0));
+    
+    // create a ground 
+    const ground = BABYLON.MeshBuilder.CreateGround("ground", {width:10, height:10});
+
+    // move box above ground 
+    box.position.y = 0.5;  //box created with default size so height is 1
+
+    // add continuous sound 
+    const sound = new BABYLON.Sound("heroes tonight", "https://www.computerhope.com/clouds.mid", scene, null, { loop: true, autoplay: true });
+
+    return scene;
+};
+
+const scene = createScene(); //Call the createScene function
+
+// Register a render loop to repeatedly render the scene
+engine.runRenderLoop(function () {
+        scene.render();
+});
+
+// Watch for browser/canvas resize events
+window.addEventListener("resize", function () {
+        engine.resize();
+});
